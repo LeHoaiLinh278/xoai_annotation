dc4850b8c1d6d9a0585ba1629ac44526905901cd
LeHoaiLinh278 <linhspkt2708@gmail.com>
2020-02-25 05:48:44 +0000

    add install database v2_1

--- install_database.sh
+++ install_database.sh
@@ -1,5 +1,5 @@
 sudo apt-get update -y
 sudo apt-get install nfs-common -y 
 mkdir -p ~/labelme_data
-mount 192.168.1.110:/home/skyteam/data/labelme_data ~/labelme_data
+sudo mount 192.168.1.110:/home/skyteam/data/labelme_data ~/labelme_data
 
